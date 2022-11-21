df18e30d2fe04da3d368f66e4dc16330149300e6
Jan-Eric Storms <jan-eric.storms@outlook.de>
2022-11-21 12:45:46 +0100

    Added Parameter

--- Hello-World/HelloWorld.java
+++ Hello-World/HelloWorld.java
@@ -3,6 +3,7 @@ Teste Mal HelloWorld isch verdret
 **/
 public class HelloWorld {
         public static void main(String[] args) {
+                String s = "Hello World";
                 System.out.println("Hello, World!");
         }
 }
