faa2230914dd1032920e1c2c07300e788e21116c
Jan-Eric Storms <jan-eric.storms@outlook.de>
2022-11-21 13:10:20 +0100

    Add

--- Hello-World/HelloWorld.java
+++ Hello-World/HelloWorld.java
@@ -4,10 +4,7 @@ Teste Mal HelloWorld isch verdret
 public class HelloWorld {
         public static void main(String[] args) {
                 String s = "Hello World";
-<<<<<<< HEAD
                 System.out.println("Hallo Welt!");
-=======
-                System.out.println(s);
->>>>>>> 66a01b8a27e450e983d639d24393701f5b3190c6
+
         }
 }
