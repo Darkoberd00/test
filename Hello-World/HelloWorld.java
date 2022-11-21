82e5c609895326a1a563c01fd7d883c675d2e0da
Jan-Eric Storms <jan-eric.storms@outlook.de>
2022-11-21 12:59:30 +0100

    Changed Hello World to Hallo Welt!

--- Hello-World/HelloWorld.java
+++ Hello-World/HelloWorld.java
@@ -4,6 +4,6 @@ Teste Mal HelloWorld isch verdret
 public class HelloWorld {
         public static void main(String[] args) {
                 String s = "Hello World";
-                System.out.println("Hello, World!");
+                System.out.println("Hallo Welt!");
         }
 }
